6a43bb60e93b0c68e62be6a7a19f637969d4c8dc
HyxiaoGe <18889592303@163.com>
2024-09-25 00:29:53 +0800

    first commit

---
+++ src/main/java/com/xiaohub/xiaohub_analysed/XiaohubAnalysedApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohub.xiaohub_analysed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaohubAnalysedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XiaohubAnalysedApplication.class, args);
+	}
+
+}
